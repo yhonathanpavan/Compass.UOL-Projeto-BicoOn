156fb5278c23405a83f1a47eb9fb0fd2f35b9650
Elaine <elaine.paula2178@gmail.com>
2022-04-10 20:34:04 -0300

    upload repository de categoria

---
+++ API-BicoOn/bicoon/src/main/java/com/compass/bicoon/repository/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.compass.bicoon.repository;
+
+import com.compass.bicoon.entities.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    Optional<Categoria> findByNome(String nome);
+}
