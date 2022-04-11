1f8e8d85c3e42a25261e0741090b0985f2aaf06e
mateusCoder <mateus.moraes0507@gmail.com>
2022-04-11 12:24:26 -0300

    Constante Sexo Deletado

--- API-BicoOn/bicoon/src/main/java/com/compass/bicoon/constants/Sexo.java
+++
@@ -1,6 +0,0 @@
-package com.compass.bicoon.contants;
-
-public enum Sexo {
-    MASCULINO,
-    FEMININO
-}
--- API-BicoOn/bicoon/src/main/java/com/compass/bicoon/repository/ClienteRespository.java
+++ API-BicoOn/bicoon/src/main/java/com/compass/bicoon/repository/ClienteRespository.java
@@ -1,2 +1,11 @@
-package com.compass.bicoon.repository;public interface ClienteRespository {
+package com.compass.bicoon.repository;
+
+import com.compass.bicoon.entities.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRespository extends JpaRepository<Cliente, Long> {
+
+    Page<Cliente> findByCidade(String cidade, Pageable paginacao);
 }
