ca86ed7b2a4a25ca1be03be4e9f132623c061278
mateusCoder <mateus.moraes0507@gmail.com>
2022-04-11 12:22:53 -0300

    Adicionado Cliente Controller

--- API-BicoOn/bicoon/src/main/java/com/compass/bicoon/controller/ClienteController.java
+++ API-BicoOn/bicoon/src/main/java/com/compass/bicoon/controller/ClienteController.java
@@ -1,2 +1,60 @@
-package com.compass.bicoon.controller;public class ClienteController {
+package com.compass.bicoon.controller;
+
+import com.compass.bicoon.dto.ClienteDto;
+import com.compass.bicoon.dto.ClienteFormDto;
+import com.compass.bicoon.entities.Cliente;
+import com.compass.bicoon.repository.ClienteRespository;
+import com.compass.bicoon.services.ClienteService;
+import com.compass.bicoon.services.ClienteServiceImpl;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.transaction.Transactional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/bicoon/cliente")
+public class ClienteController {
+
+    @Autowired
+    ClienteServiceImpl clienteService;
+
+    @Autowired
+    ModelMapper mapper;
+
+    @GetMapping
+    public ResponseEntity<Page<ClienteDto>> listarClientes(@RequestParam(required = false) String cidade,
+                                                          @PageableDefault(sort = "id", direction = Sort.Direction.ASC, size = 100) Pageable paginacao){
+        return ResponseEntity.ok().body(clienteService.findAll(cidade, paginacao));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteDto> detalhesCliente(@PathVariable Long id){
+        return ResponseEntity.ok().body(clienteService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ClienteDto> cadastrarCliente(@RequestBody ClienteFormDto clienteFormDto, UriComponentsBuilder uriBuilder){
+        return ResponseEntity.ok().body(clienteService.create(clienteFormDto, uriBuilder));
+    }
+
+    @Transactional
+    @PutMapping("/{id}")
+    public ResponseEntity<ClienteDto> atualizarCliente(@PathVariable Long id, @RequestBody ClienteFormDto clienteFormDto){
+        return ResponseEntity.ok().body(clienteService.update(id, clienteFormDto));
+    }
+
+    @Transactional
+    @DeleteMapping("/{id}")
+    public String deletarCliente(@PathVariable Long id){
+        return clienteService.delete(id);
+    }
 }
