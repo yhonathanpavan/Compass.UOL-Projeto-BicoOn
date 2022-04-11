3a3ee8ec7f5f43ddd33723f6d722c3ff960b5be2
mateusCoder <mateus.moraes0507@gmail.com>
2022-04-11 12:21:18 -0300

    Adicionado DTO do Cliente

--- API-BicoOn/bicoon/src/main/java/com/compass/bicoon/dto/ClienteDto.java
+++ API-BicoOn/bicoon/src/main/java/com/compass/bicoon/dto/ClienteDto.java
@@ -1,2 +1,20 @@
-package com.compass.bicoon.dto;public class ClienteDto {
+package com.compass.bicoon.dto;
+
+
+import com.compass.bicoon.constants.Sexo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Data
+public class ClienteDto {
+
+    private Long id;
+    private String nome;
+    private String cidade;
+    @Enumerated(EnumType.STRING)
+    private Sexo sexo;
 }
--- API-BicoOn/bicoon/src/main/java/com/compass/bicoon/dto/ClienteFormDto.java
+++ API-BicoOn/bicoon/src/main/java/com/compass/bicoon/dto/ClienteFormDto.java
@@ -1,2 +1,16 @@
-package com.compass.bicoon.dto;public class ClienteFormDto {
+package com.compass.bicoon.dto;
+
+import com.compass.bicoon.constants.Sexo;
+import lombok.Data;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Data
+public class ClienteFormDto {
+
+    private String nome;
+    private String cidade;
+    @Enumerated(EnumType.STRING)
+    private Sexo sexo;
 }
