4da5c15b1ad67f5510ab6e64510d3957fdfd789b
mateusCoder <mateus.moraes0507@gmail.com>
2022-04-11 12:22:12 -0300

    Adicionado Entidade Cliente

--- API-BicoOn/bicoon/src/main/java/com/compass/bicoon/entities/Cliente.java
+++ API-BicoOn/bicoon/src/main/java/com/compass/bicoon/entities/Cliente.java
@@ -1,2 +1,14 @@
-package com.compass.bicoon.entities;public class Cliente {
+package com.compass.bicoon.entities;
+
+import com.compass.bicoon.model.Usuario;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+public class Cliente extends Usuario {
+
 }
