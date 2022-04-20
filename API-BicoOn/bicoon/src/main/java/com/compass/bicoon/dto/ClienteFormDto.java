721f66bcdede667cc4fca75716b02e0184c377a0
mateusCoder <mateus.moraes0507@gmail.com>
2022-04-20 11:36:32 -0300

    Adicionado Builder para Clientes

--- API-BicoOn/bicoon/src/main/java/com/compass/bicoon/dto/ClienteFormDto.java
+++ API-BicoOn/bicoon/src/main/java/com/compass/bicoon/dto/ClienteFormDto.java
@@ -1,12 +1,18 @@
 package com.compass.bicoon.dto;
 
 import com.compass.bicoon.constants.Sexo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClienteFormDto {
 
     @NotNull @NotEmpty
--- API-BicoOn/bicoon/src/main/java/com/compass/bicoon/entities/Cliente.java
+++ API-BicoOn/bicoon/src/main/java/com/compass/bicoon/entities/Cliente.java
@@ -1,7 +1,8 @@
 package com.compass.bicoon.entities;
 
+import com.compass.bicoon.constants.Sexo;
 import com.compass.bicoon.model.Usuario;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,16 @@ import javax.persistence.Entity;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
 public class Cliente extends Usuario {
 
+    public Cliente(long id, String nome, String email, String senha, String cidade, Sexo sexo) {
+        super.id = id;
+        super.nome = nome;
+        super.email = email;
+        super.senha = senha;
+        super.cidade = cidade;
+        super.sexo = sexo;
+    }
 }
