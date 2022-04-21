1ca3d883784ff396e91cbba2bdbc20764cc85046
Yhonathan Pavan <yhonathannpavan@gmail.com>
2022-04-21 17:12:52 -0300

    Adicionado anotações do Lombok para funcionamento dos testes

--- API-BicoOn/bicoon/src/main/java/com/compass/bicoon/dto/ServicoDto.java
+++ API-BicoOn/bicoon/src/main/java/com/compass/bicoon/dto/ServicoDto.java
@@ -1,10 +1,16 @@
 package com.compass.bicoon.dto;
 
 import com.compass.bicoon.entities.Categoria;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ServicoDto {
 
     private Long id;
--- API-BicoOn/bicoon/src/main/java/com/compass/bicoon/entities/Categoria.java
+++ API-BicoOn/bicoon/src/main/java/com/compass/bicoon/entities/Categoria.java
@@ -1,6 +1,9 @@
 package com.compass.bicoon.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +12,9 @@ import javax.persistence.Id;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Categoria {
 
     @Id
