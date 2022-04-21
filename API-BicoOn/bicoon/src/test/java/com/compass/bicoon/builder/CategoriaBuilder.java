cc4e2d1ff200e960128abfc3a7c05a6cc20b4af0
Elaine <elaine.paula2178@gmail.com>
2022-04-21 19:25:48 -0300

    Realizado testes de categoria

---
+++ API-BicoOn/bicoon/src/test/java/com/compass/bicoon/builder/CategoriaBuilder.java
@@ -0,0 +1,44 @@
+package com.compass.bicoon.builder;
+
+import com.compass.bicoon.dto.CategoriaDto;
+import com.compass.bicoon.dto.CategoriaFormDto;
+import com.compass.bicoon.entities.Categoria;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
+import java.util.Arrays;
+
+public final class CategoriaBuilder {
+
+    public static final Long ID = 1L;
+    public static final String NOME = "Babá";
+
+    public static Categoria getCategoria(){
+        return  Categoria.builder()
+                .id(ID)
+                .nome(NOME)
+                .build();
+    }
+
+    public static CategoriaDto getCategoriaDto(){
+        return  CategoriaDto.builder()
+                .id(ID)
+                .nome(NOME)
+                .build();
+    }
+
+    public static CategoriaFormDto getCategoriaForm(){
+        return  CategoriaFormDto.builder()
+                .nome(NOME)
+                .build();
+    }
+
+    public static Page<Categoria> getCategoriaPaginacao(){
+        return new PageImpl<>(Arrays.asList(getCategoria(), getCategoria()));
+    }
+
+    public static Page<CategoriaDto> getCategoriaPaginacaoDto(){
+        return new PageImpl<>(Arrays.asList(getCategoriaDto(), getCategoriaDto()));
+    }
+
+}
---
+++ API-BicoOn/bicoon/src/test/java/com/compass/bicoon/controllers/CategoriaControllerTest.java
@@ -0,0 +1,95 @@
+package com.compass.bicoon.controllers;
+
+import com.compass.bicoon.builder.CategoriaBuilder;
+import com.compass.bicoon.dto.CategoriaFormDto;
+import com.compass.bicoon.repository.CategoriaRepository;
+import com.compass.bicoon.services.CategoriaServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(controllers = CategoriaController.class)
+class CategoriaControllerTest {
+
+    @MockBean
+    private CategoriaRepository repository;
+
+    @MockBean
+    private CategoriaServiceImpl service;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper mapper;
+
+
+    @Test
+    void listarCategorias() throws Exception {
+
+        when(service.listarCategorias(any(PageRequest.class))).thenReturn(CategoriaBuilder.getCategoriaPaginacaoDto());
+
+        mockMvc.perform(
+                        get("/bicoon/categorias")
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content.[0].id").value(1))
+                .andExpect(jsonPath("$.content.[0].nome").value("Babá"));
+    }
+
+
+    @Test
+    void atualizarCategoria() throws Exception {
+
+        when(service.atualizarCategoria(anyLong(),any(CategoriaFormDto.class))).thenReturn(CategoriaBuilder.getCategoriaDto());
+
+        MockHttpServletRequestBuilder mockRequest = put("/bicoon/categorias/{id}", 1L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(CategoriaBuilder.getCategoriaForm()));
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$.nome").value("Babá"));
+    }
+
+    @Test
+    void CadastrarCategoria() throws Exception {
+
+        when(service.cadastrarCategoria(any(CategoriaFormDto.class))).thenReturn(any());
+
+        MockHttpServletRequestBuilder mockRequest = post("/bicoon/categorias/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(CategoriaBuilder.getCategoriaForm()));
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void deletarCategoria() throws Exception {
+
+        when(repository.findById(anyLong())).thenReturn(Optional.of(CategoriaBuilder.getCategoria()));
+
+        mockMvc.perform( delete("/bicoon/categorias/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
