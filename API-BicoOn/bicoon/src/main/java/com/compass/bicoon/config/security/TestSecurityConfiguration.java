149af9794f39286c64a6ee3d69da8197ae77111a
Alan <alanfernando2809@gmail.com>
2022-04-28 13:10:04 -0300

    Adicionando TestConfig

---
+++ API-BicoOn/bicoon/src/main/java/com/compass/bicoon/config/security/TestSecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.compass.bicoon.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+@Profile("test")
+public class TestSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and().csrf().disable();
+    }
+
+}
